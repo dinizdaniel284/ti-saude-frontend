b6ed8f7890fa5a9f65ddd265dbc7dd310bd1573c
dinizdaniel284 <dinizdaniel284@gmail.com>
2025-04-11 19:10:34 -0300

    Create Main.jsx

---
+++ components/Main.jsx
@@ -0,0 +1,15 @@
+// components/Main.jsx
+import React from "react";
+
+const Main = () => {
+  return (
+    <main className="p-4 text-center">
+      <h2 className="text-2xl font-semibold mb-4">Bem-vindo ao TI-Saúde</h2>
+      <p className="text-gray-600">
+        Este site é dedicado a ajudar profissionais da saúde a migrarem para a área de Tecnologia da Informação. Aqui você encontrará quizzes, conteúdos informativos e ferramentas úteis para essa transição!
+      </p>
+    </main>
+  );
+};
+
+export default Main;
