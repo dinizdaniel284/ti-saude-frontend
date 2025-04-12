0413ba0c722e786b969392f3042ba733e00581c4
dinizdaniel284 <dinizdaniel284@gmail.com>
2025-04-11 23:27:26 -0300

    Update quiz.tsx

--- pages/quiz.tsx
+++ pages/quiz.tsx
@@ -54,6 +54,11 @@ export default function Quiz() {
           Quiz TI-Saúde
         </h1>
 
+        {/* Contador de Perguntas */}
+        <p className="text-lg text-gray-600 mb-4">
+          Pergunta {currentQuestionIndex + 1} de {questions.length}
+        </p>
+
         <div className="space-y-6">
           <p className="text-xl text-gray-800">{questions[currentQuestionIndex].question}</p>
 
@@ -61,9 +66,9 @@ export default function Quiz() {
             {questions[currentQuestionIndex].options.map((option, index) => (
               <button
                 key={index}
-                className={`w-full py-3 px-4 border rounded-lg text-lg ${
+                className={`w-full py-3 px-4 border rounded-lg text-lg transition-all duration-300 ease-in-out transform ${
                   selectedOption === index
-                    ? "bg-blue-600 text-white"
+                    ? "bg-blue-600 text-white scale-105"
                     : "bg-gray-100 hover:bg-blue-500"
                 }`}
                 onClick={() => handleOptionChange(index)}
@@ -76,12 +81,29 @@ export default function Quiz() {
           <div className="mt-6">
             <button
               onClick={handleNextQuestion}
-              className="bg-blue-600 text-white font-semibold py-3 px-6 rounded-lg hover:bg-blue-800 w-full"
+              className="bg-blue-600 text-white font-semibold py-3 px-6 rounded-lg hover:bg-blue-800 w-full transition-all duration-300 ease-in-out transform hover:scale-105"
             >
               {currentQuestionIndex < questions.length - 1 ? "Próxima Pergunta" : "Finalizar Quiz"}
             </button>
           </div>
         </div>
+
+        {/* Tela de Conclusão */}
+        {currentQuestionIndex === questions.length && (
+          <div className="mt-6 text-center">
+            <h2 className="text-2xl font-semibold text-gray-900 mb-4">
+              Parabéns! Você completou o Quiz.
+            </h2>
+            <p className="text-lg text-gray-700">Aqui estão suas respostas:</p>
+            <ul className="mt-4 space-y-2 text-left text-gray-800">
+              {answers.map((answer, index) => (
+                <li key={index} className="text-xl">
+                  Pergunta {index + 1}: {answer}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
     </div>
   );
