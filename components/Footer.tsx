567e7d1385bd059d19a6761f2779fda9ab7bae96
dinizdaniel284 <dinizdaniel284@gmail.com>
2025-04-11 23:11:05 -0300

    Update and rename Footer.jsx to Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,6 @@
-// components/Footer.jsx
 import React from "react";
 
-const Footer = () => {
+const Footer: React.FC = () => {
   return (
     <footer className="bg-gray-100 p-4 text-center text-sm text-gray-600 mt-8">
       © {new Date().getFullYear()} TI-Saúde. Todos os direitos reservados.
