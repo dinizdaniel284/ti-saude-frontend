5c3c4f7252ae3ecaf64fa5a3711813692a1b915c
dinizdaniel284 <dinizdaniel284@gmail.com>
2025-04-12 00:43:04 -0300

    Delete pages/pages directory

--- pages/pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-// pages/_app.tsx
-import "../styles/globals.css"; // Caminho relativo correto
-import type { AppProps } from "next/app";
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
