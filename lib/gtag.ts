caf57f3f12ee973fe123a4e1b08fc28db6771f78
dinizdaniel284 <dinizdaniel284@gmail.com>
2025-05-12 00:20:49 -0300

    Update gtag.ts
    Atualização da função de visualização de página no gtag

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -1,10 +1,9 @@
-// lib/gtag.ts
-
+// Pega o ID do GA do arquivo .env (com prefixo NEXT_PUBLIC_)
 export const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_ID || '';
 
 // Função para registrar visualização de página
 export const pageview = (url: string) => {
-  if (typeof window !== 'undefined' && typeof window.gtag === 'function') {
+  if (typeof window !== 'undefined' && window.gtag) {
     window.gtag('config', GA_MEASUREMENT_ID, {
       page_path: url,
     });
