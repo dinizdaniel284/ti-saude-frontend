f6c7e99c575b5ca7560f86c355dee41cd1df3a4b
dinizdaniel284 <dinizdaniel284@gmail.com>
2025-05-12 00:12:47 -0300

    Update gtag.ts

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -1,11 +1,12 @@
 // lib/gtag.ts
 
-// Pega o ID do GA do arquivo .env (com prefixo NEXT_PUBLIC_)
 export const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_ID || '';
 
 // Função para registrar visualização de página
 export const pageview = (url: string) => {
-  window.gtag('config', GA_MEASUREMENT_ID, {
-    page_path: url,
-  });
+  if (typeof window !== 'undefined' && typeof window.gtag === 'function') {
+    window.gtag('config', GA_MEASUREMENT_ID, {
+      page_path: url,
+    });
+  }
 };
