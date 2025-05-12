5156706f3eebb349de7e2e42d02311a74fa85bb8
dinizdaniel284 <dinizdaniel284@gmail.com>
2025-05-12 16:00:33 -0300

    Update privacidade.tsx

--- pages/privacidade.tsx
+++ pages/privacidade.tsx
@@ -1,1 +1,16 @@
-"" 
+import React from 'react';
+
+export default function PrivacidadePage() {
+  return (
+    <div>
+      <h1>Política de Privacidade</h1>
+      <p>
+        Respeitamos sua privacidade e garantimos a proteção dos seus dados. Todas as informações coletadas no site 
+        <strong> TI & Saúde </strong> são utilizadas exclusivamente para melhorar sua experiência e oferecer conteúdo relevante.
+      </p>
+      <p>
+        Não compartilhamos dados com terceiros sem seu consentimento e seguimos boas práticas de segurança da informação.
+      </p>
+    </div>
+  );
+}
