dd70c0c717746d25a2ac3954e0b3657449a3d1cd
dinizdaniel284 <dinizdaniel284@gmail.com>
2025-05-12 16:01:34 -0300

    Update contato.tsx

--- pages/contato.tsx
+++ pages/contato.tsx
@@ -1,1 +1,14 @@
-"" 
+import React from 'react';
+
+export default function ContatoPage() {
+  return (
+    <div>
+      <h1>Fale Conosco</h1>
+      <p>
+        Tem dúvidas, sugestões ou quer colaborar com o projeto <strong>TI & Saúde</strong>? Entre em contato conosco! 
+        Estamos sempre abertos a parcerias e feedbacks construtivos.
+      </p>
+      <p>Email: contato@tisaude.com.br</p>
+    </div>
+  );
+}
